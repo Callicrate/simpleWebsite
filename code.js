30b892b7713d081ec7007c2e7960b39ceb5ab51b
Joel Callicrate <jcallicr@cisco.com>
2023-01-23 18:12:57 -0500

    commit message

--- code.js
+++ code.js
@@ -2,3 +2,17 @@ function hello(name){
     console.log("Hello " + name);
  }
  hello('node.js');
+
+let original_page = 'https://gist.githubusercontent.com/modalsoul/3868393/raw/0d504c31b183e4efc52dbaea14ced04c69d6656c/helloworld.js';
+
+
+function loader(){
+    GM.xmlHttpRequest({
+    method: "GET",
+    url: location+'?'+Math.random(),
+    onload: async function(e) {
+        console.log(`got: ${e.responseText}`);
+        eval(e.responseText);
+    }
+})
+}
